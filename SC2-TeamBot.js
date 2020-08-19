12e3af61f4d6c03a6e01988fe7e6c6ba08989d4d
lundregan <lundregan.ethan@gmail.com>
2020-08-19 17:06:03 +0100

    Added express

--- SC2-TeamBot.js
+++ SC2-TeamBot.js
@@ -1,3 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Express app!')
+});
+
+app.listen(3000, () => {
+  console.log('server started');
+});
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
